9e99d695e3e264805c12e063134ced780569b071
malek mekdashi <malekmekdashi@gmail.com>
2022-09-13 22:19:44 -0700

    code refactor

--- template.js
+++ template.js
@@ -1,13 +1,13 @@
 
 const manager = managerData => {
     return `
-    <div id="${managerData.getRole()}-card" class= "box card">
-        <div class = "box name-role manager-name">
+    <div id="${managerData.getRole()}">
+        <div class = "name-role manager-name">
             <h2>${managerData.getName()}</h2>
             <h3>Role: ${managerData.getRole()}</h3>
         </div>
-        <div class ="box employee-info">
-            <ul class="list-group">
+        <div>
+            <ul>
                 <li class = "list-group-item">ID: ${managerData.getId()}</li>
                 <li class = "list-group-item">Email: <a href= "mailto:${managerData.getEmail()}">${managerData.getEmail()}</a></li>
                 <li class = "list-group-item">Office Number: ${managerData.getOfficeNumber()}</li>
@@ -19,13 +19,13 @@ const manager = managerData => {
 
 const engineer = engineerData => {
     return `
-    <div id = "${engineerData.getRole()}-card" class = "box card">
-        <div class = "box name-role engineer-name">
+    <div id = "${engineerData.getRole()}>
+        <div class = "name-role engineer-name">
             <h2>${engineerData.getName()}</h2>
             <h3>Role: ${engineerData.getRole()}</h3>
         </div>
-        <div class ="box employee-info">
-            <ul class="list-group">
+        <div>
+            <ul>
                 <li class = "list-group-item">ID: ${engineerData.getId()}</li>
                 <li class = "list-group-item">Email: <a href= "mailto:${engineerData.getEmail()}">${engineerData.getEmail()}</a></li>
                 <li class = "list-group-item">Github: <a href= "https://www.github.com/${engineerData.getGithub()}">${engineerData.getGithub()}</a></li>
@@ -37,13 +37,13 @@ const engineer = engineerData => {
 
 const intern = internData => {
     return `
-    <div id = "${internData.getRole()}-card" class = "box card">
-        <div class ="box name-role intern-name">
+    <div id = "${internData.getRole()}>
+        <div class ="name-role intern-name">
             <h2>${internData.getName()}</h2>
             <h3>Role: ${internData.getRole()}</h3>
         </div>
-        <div class ="box employee-info">
-            <ul class="list-group">
+        <div>
+            <ul>
                 <li class = "list-group-item">ID: ${internData.getId()}</li>
                 <li class = "list-group-item">Email: <a href= "mailto:${internData.getEmail()}">${internData.getEmail()}</a></li>
                 <li class = "list-group-item">School: ${internData.getSchool()}</li>
@@ -78,12 +78,12 @@ return `
     <title>Team Profile Generator</title>
 </head>
 <body>
-    <header class = "container">
-        <div class = "flex box justify center my-team">
-            <h1>Team</h1>
+    <header>
+        <div class = "my-team">
+            <h1>Team Birria</h1>
         </div>
     </header>
-    <main class = "container">
+    <main>
     ${employeesClass(data)}
     </main>
 </body>
